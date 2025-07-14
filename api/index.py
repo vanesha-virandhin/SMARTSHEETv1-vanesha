a144752f652bda9793b04c9a1ae60bde47fc4517
vanesha-virandhin <pkm54@teknokrat.ac.id>
2025-07-14 16:25:03 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -129,7 +129,7 @@ class ArtikelAPI:
 
 
 config_artikel = ArtikelAPI()
-config_artikel.sheet_id('14EXDQFZ3w3m20W1w3EBl0MGCO9hxNz63ZWhC4tQewYY')
+config_artikel.sheet_id('1ZXgSAT5dm15EVPQfLEoN-wQlhWuLQ5g0bW-3I1J7bU4')
 config_artikel.gid('710523043')
 
 
